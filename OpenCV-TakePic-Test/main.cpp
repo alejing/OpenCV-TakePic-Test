21630fcbca74477d29daa76e24b89c2ed4378878
Alejandro Franco <alejing@gmail.com>
2016-03-24 13:57:08 -0500

    Toma Foto OpenCV
    Muestra video desde cámara de elección y toma una foto

--- OpenCV-TakePic-Test/main.cpp
+++ OpenCV-TakePic-Test/main.cpp
@@ -6,10 +6,51 @@
 //  Copyright © 2016 Alejandro Franco. All rights reserved.
 //
 
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+using namespace std;
+using namespace cv;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+String inttostr(int input)
+{
+    stringstream ss;
+    ss << input;
+    return ss.str();
+}
+
+int main(void)
+{
+    Mat frame;
+    
+    vector<int> compression_params;
+    // vector that stores the compression parameters of the image
+    compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+    // specify the compression technique
+    compression_params.push_back(100); //specify the compression quality
+    // initialize image counter
+    int photocount = 0;
+    String imagename;
+    int key;
+    // open camera no.0  0=internal 1=external
+    VideoCapture cap(1);
+    // wait 30 milliseconds and check for "s" key
+    while ((key = waitKey(30)) != 115)
+    {
+        // save captured image to frame variable
+        cap >> frame;
+        // show image on window named Camara
+        imshow("Camara", frame);
+        if (key == 'c')
+        {
+            // increment image number for each capture
+            photocount++;
+            imshow("Imagen capturada", frame);
+            
+            imagename = "/Users/ImagenesOpenCV/foto " + inttostr(photocount) + ".jpg";
+            // finalize imagename to save
+            imwrite(imagename, frame, compression_params);
+            
+        }
+        
+    }
 }
